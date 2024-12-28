89109812d87b6b7144d159f6f40bbbd7e8d78c08
Zasmooncat <zasmomoxipol@gmail.com>
2024-12-28 13:40:02 +0000

    jumbocarousel

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,7 +5,7 @@ import zpdalogo from "../../img/zepdalogo.png"
 export const Navbar = () => {
 	return (
 	<>
-	<nav className="navbar navbar-expand-lg bg-body-white">
+	<nav className="container navbar navbar-expand-lg bg-body-white">
   <div className="container-fluid">
     <Link to={"/"}className="navbar-logo"><img src={zpdalogo}/></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
