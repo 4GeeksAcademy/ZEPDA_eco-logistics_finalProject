dd56d981c0e686ee5f915d00e23580fb7b47222a
Zasmooncat <zasmomoxipol@gmail.com>
2025-01-10 13:19:17 +0000

    pagina de servicios

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -31,9 +31,9 @@ export const Home = () => {
 			En esta plataforma podrás encontrar soluciones sostenibles para que tu empresa sea más eco-friendly. 
 			Podrás encontrar servicios que te ayudarán a disminuir el impacto ambiental de tu empresa. Además, 
 			podrás conocer el impacto positivo que generas al adquirir estos productos y servicios.
-			Sólo tienes que crear una cuenta en nuestra plataforma y empezar a disfrutar de los beneficios de ser eco-friendly.
+			Sólo tienes que crear una cuenta en nuestra plataforma y empezar a disfrutar de los beneficios de ser eco-friendly. Súmate al cambio!
 			</p>
-			<button className="btn">súmate al cambio</button>
+			
 
 			
 
