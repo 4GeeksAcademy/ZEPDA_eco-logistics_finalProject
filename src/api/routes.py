c3eab94e20c1de8e5167f9465fa21c2be1ff15ed
Zasmooncat <zasmomoxipol@gmail.com>
2025-01-21 14:07:57 +0000

    rutas companies y companybyID creadas

--- src/api/routes.py
+++ src/api/routes.py
@@ -272,45 +272,21 @@ if __name__ == '__main__':
 
 
 
-# @api.route('/registerCompany', methods=['POST'])
-# def register_company():
-    
-#     body = request.get_json()
-#     company_nif = body.get('nif', None)
-#     company_nombre = body.get('nombre', None)
-#     company_sector = body.get('sector', None)
-#     company_direccion = body.get('direccion', None)
-#     company_email = body.get('email', None)
-#     company_descripcion = body.get('descripcion', None)
-#     company_web= body.get('web', None)
-#     company_contraseña = body.get('contraseña', None)
-#     company_certificado = body.get('certificado', None)
-#     if company_nombre is None or company_email is None or company_contraseña is None:
-#         return {'message': 'Missing arguments'}      
-#     bpassword = bytes(company_contraseña, 'utf-8')
-#     salt = bcrypt.gensalt(14)
-#     hashed_password = bcrypt.hashpw(password=bpassword, salt=salt)       
-#     user = Company(company_nif, company_nombre, company_sector, company_direccion, company_email, company_descripcion, company_web, hashed_password.decode('utf-8'), company_certificado)    
-#     #return {'message': f'nombre: {user.nombre} email: {user.email} contraseña: {contraseña}'}
-#     db.session.add(user)
-#     db.session.commit()
-#     return {'message': f'User {user.email} was created'}
-
-# @api.route('/profile/companies', methods=['GET'])
-# def get_companies():
-#     """Obtiene todas las compañías"""
-#     companies = Company.query.all()
-#     companies_serialized = [company.serialize() for company in companies]
-#     return jsonify(companies_serialized), 200
 
 
-# @api.route('/profile/companies/<int:company_id>', methods=['GET'])
-# def get_company(company_id):
-#     """Obtiene una compañía por ID"""
-#     company = Company.query.get(company_id)
-#     if company is None:
-#         return jsonify({"message": "Company not found"}), 404
-#     return jsonify(company.serialize()), 200
+@api.route('/companies', methods=['GET'])
+def get_companies():
+    companies = Company.query.all()
+    companies_serialized = [company.serialize() for company in companies]
+    return jsonify(companies_serialized), 200
+
+
+@api.route('/companies/<int:company_id>', methods=['GET'])
+def get_company_byID(company_id):
+    company = Company.query.get(company_id)
+    if company is None:
+        return jsonify({"message": "Company not found"}), 404
+    return jsonify(company.serialize()), 200
 
 
 # @api.route('/profile/companies', methods=['POST'])
