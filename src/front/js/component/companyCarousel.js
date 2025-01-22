642d29beea65b9e3466ef1e317bedfc47f9ea139
Zasmooncat <zasmomoxipol@gmail.com>
2025-01-22 16:52:22 +0000

    arreglado transporteS

--- src/front/js/component/companyCarousel.js
+++ src/front/js/component/companyCarousel.js
@@ -36,7 +36,7 @@ export const CompanyCarousel = ({sector, isGrid}) => {
         switch(sectorId){
             case 'Packaging':
                 return <img className="imgservicios" src="../packaging-sostenible.webp" alt="img-packaging" />;
-            case 'Transporte':
+            case 'Transportes':
                 return <img className="imgservicios" src="../furgonetas-w.webp" alt="img-transporte" />;
             case 'Gestión de Residuos':
                 return <img className="imgservicios" src="../1533057826357.jpeg" alt="img-gestion-residuos" />;
