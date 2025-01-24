b760050c574341b064c15ecb45d0e3012053a366
Sajadev404 <empresasarn@gmail.com>
2025-01-24 14:57:53 +0000

    funcion de correo a empresa

--- src/front/js/component/hiringContrarion.js
+++ src/front/js/component/hiringContrarion.js
@@ -1,27 +1,58 @@
 import React, { useState, useContext } from "react";
 import { Button, Form, Alert } from "react-bootstrap";
 import { Context } from "../store/appContext";
+import emailjs from "emailjs-com"; // Importa emailjs
 
-export const HiringContration = ({ handleClose, company}) => {
+export const HiringContration = ({ handleClose, company }) => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
     const [success, setSuccess] = useState("");
     const [error, setError] = useState("");
-console.log(company)
+
     const { actions, store } = useContext(Context);
+
+    const service_id = process.env.EMAILJS_SERVICE_ID;
+    const user_id = process.env.EMAILJS_USER_ID;
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         try {
-            // Llamar a la acción addHiring desde el flux
+           
             const isAdded = await actions.addHiring(company.id, store.profile.id);
             if (isAdded) {
                 setSuccess("Hemos enviado un correo al servicio que ha solicitado");
+
+               
+                const templateParams = {
+                    from_name: name,
+                    from_email: email,
+                    to_name: company.nombre,
+                    to_email: company.email,
+                    message: message,
+                };
+
+              
+                emailjs
+                    .send(service_id, "template_h5b5bie", templateParams, user_id)
+                    .then(
+                        (response) => {
+                            console.log("Email sent successfully", response.status, response.text);
+                            setSuccess("Correo enviado correctamente.");
+                            setError("");
+                        },
+                        (error) => {
+                            console.error("Error sending email:", error);
+                            setError("Hubo un error al enviar el correo. Inténtelo de nuevo más tarde.");
+                            setSuccess("");
+                        }
+                    );
+
+               
                 setTimeout(() => {
                     setSuccess("");
-                    handleClose(); // Cerrar el modal después de mostrar el mensaje de éxito
+                    handleClose();
                 }, 2000);
             } else {
                 setError("Error al realizar la contratación, intente nuevamente.");
@@ -32,11 +63,10 @@ console.log(company)
         }
     };
 
-    
-
     return (
         <>
             {success && <Alert variant="success">{success}</Alert>}
+            {error && <Alert variant="danger">{error}</Alert>}
             <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="formName">
                     <Form.Label>Nombre</Form.Label>
@@ -66,7 +96,7 @@ console.log(company)
                         required
                     />
                 </Form.Group>
-                <Button variant="btn" type="submit" className="mt-3" >
+                <Button variant="btn" type="submit" className="mt-3">
                     Enviar
                 </Button>
             </Form>
